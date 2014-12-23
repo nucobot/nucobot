f1931e1f6bc8e7ace71312a17a8c3c9dce633b15
Vsevolod Livinskij <vsevolod.livinskij@frtk.ru>
2014-12-23 19:57:30 +0300

    New node (wheel_driver)

---
+++ src/wheel_driver/wheel_driver.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "wheel_driver");
+  ros::NodeHandle nh;
+
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+  ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
+
+  ros::Rate loop_rate(10);
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+  return 0;
+}
