bc486e6aaca5d823c3a1c316b4687ab4b53cb134
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-12-25 15:38:02 +0300

    cylinders are now seen in Rviz

--- src/fake_towermap/fake_towermap.py
+++ src/fake_towermap/fake_towermap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import re
-import rospy
+import rospy, tf
 from std_msgs.msg import String
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
@@ -9,33 +9,23 @@ from gazebo_msgs.msg import ModelStates
 
 
 pub = rospy.Publisher('visualization/fake_towermap/objects', MarkerArray, queue_size=10)
-
+br = tf.TransformBroadcaster()
        
 
 
 def callback(data):
     ma = MarkerArray()
-    
-
-
     for i, name in enumerate(data.name):
         marker = Marker()
-        marker.header.frame_id = "base_link"
+        marker.header.frame_id = "world"
         marker.ns = "toys"
         marker.header.stamp = rospy.Time()
         marker.action = Marker.ADD
 
         if 'cylinder' in name:
-
-
-            
             marker.type = Marker.CYLINDER
             marker.pose = data.pose[i]
-            marker.id = 10 * marker.type + int(re.split('_*', name)[-1])
- 
-            rospy.loginfo(rospy.get_caller_id() + " " +name+ " " + str(marker.id))
-            rospy.loginfo(rospy.get_caller_id() + " " + str(marker.pose))
-            
+            marker.id = 10 * marker.type + int(re.split('_*', name)[-1])           
             marker.scale.x = 0.06
             marker.scale.y = 0.06
             marker.scale.z = 0.07
@@ -44,18 +34,27 @@ def callback(data):
             marker.color.g = 1.0
             marker.color.b = 0.0
             ma.markers.append(marker)
-            #rospy.loginfo(rospy.get_caller_id() + marker.ns)
-
-    
-    for m in ma.markers:
-        rospy.loginfo(rospy.get_caller_id() + " " + str(len(ma.markers)))
-        rospy.loginfo(rospy.get_caller_id() + " " + str(m.id))
-        rospy.loginfo(rospy.get_caller_id() + " " + str(m.pose))
+        
+        if 'nucobot' in name:
+            br.sendTransform((data.pose[i].position.x, data.pose[i].position.y, data.pose[i].position.z),
+                     (data.pose[i].orientation.x, data.pose[i].orientation.y, data.pose[i].orientation.z, data.pose[i].orientation.w),
+                     rospy.Time.now(),
+                     "base_footprint",
+                     "world")    
 
     pub.publish(ma)
-    
+
+
+
+
+
 
 if __name__ == '__main__':
     rospy.init_node('fake_towermap', anonymous=False)
     rospy.Subscriber("/gazebo/model_states", ModelStates, callback)
     rospy.spin()
+
+
+
+
+
