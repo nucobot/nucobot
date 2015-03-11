127bebb8a0bd2bf43f552c3b0496eda0f05ed782
Thar0l <gusarov.aa@gmail.com>
2015-03-12 00:41:42 +0300

    Added battery voltage monitor

--- firmware/nucobot_arduino_VNH5019/nucobot_arduino_VNH5019.ino
+++ firmware/nucobot_arduino_VNH5019/nucobot_arduino_VNH5019.ino
@@ -4,15 +4,19 @@
  #include <WProgram.h>
 #endif
 
+#include <math.h>
+
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/Float32.h>
 #include "DualVNH5019MotorShield.h"
 
 ros::NodeHandle nh;
 DualVNH5019MotorShield md;
 geometry_msgs::Point wheel_odom_msg;
-long int publish_timer, motor_1_timer, motor_2_timer;
+long int md_publish_timer, bat_publish_timer, motor_1_timer, motor_2_timer;
+std_msgs::Float32 bat1_msg, bat2_msg;
 
 #define WHEEL_SEPARATION 0.17 // Wheel separation in meters
 #define TRANSLATION_FACTOR 400 // The speed in m/s is multiplied by this factor to get the PWM 
@@ -21,6 +25,9 @@ long int publish_timer, motor_1_timer, motor_2_timer;
 #define MOTOR1_REV_PIN 4
 #define MOTOR2_INT 0
 #define MOTOR2_REV_PIN 5
+
+#define BAT1_PIN 0
+#define BAT2_PIN 1
 
 void motor_1_cb(){
   if (digitalRead(MOTOR1_REV_PIN) == HIGH) {
@@ -57,6 +64,8 @@ void motor_cb(const geometry_msgs::Twist& cmd_msg){
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", motor_cb);
 ros::Publisher pub_wheel_odom("raw_wheel_odometry", &wheel_odom_msg);
+ros::Publisher pub_bat1("battery_voltage/1", &bat1_msg);
+ros::Publisher pub_bat2("battery_voltage/2", &bat2_msg);
 
 void setup()
 {
@@ -65,6 +74,8 @@ void setup()
   md.init();
   nh.initNode();
   nh.advertise(pub_wheel_odom);
+  nh.advertise(pub_bat1);
+  nh.advertise(pub_bat2);
   nh.subscribe(sub);
   attachInterrupt(MOTOR1_INT, motor_1_cb, RISING);
   attachInterrupt(MOTOR2_INT, motor_2_cb, RISING);
@@ -72,13 +83,20 @@ void setup()
 
 void loop()
 {
-  if ( (millis()-publish_timer) > 50){
+  if ( (millis()-md_publish_timer) > 50){
     wheel_odom_msg.x = motor_2_timer;
     wheel_odom_msg.y = -motor_1_timer;
     motor_1_timer = 0;
     motor_2_timer = 0;
     pub_wheel_odom.publish(&wheel_odom_msg);
-    publish_timer = millis();
+    md_publish_timer = millis();
+  }
+  if ( (millis()-bat_publish_timer) > 1000){
+    bat1_msg.data = round(((analogRead(BAT1_PIN))*15000.0/1024.0));
+    bat2_msg.data = round(((analogRead(BAT2_PIN))*15000.0/1024.0));
+    pub_bat1.publish(&bat1_msg);
+    pub_bat2.publish(&bat2_msg);
+    bat_publish_timer = millis();
   }
   nh.spinOnce();
 }
